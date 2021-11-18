0f21972e19c1475349656331ba2aea0151c6be61
Petr Ustyuzhanin <petrakliy@gmail.com>
2021-11-18 19:42:28 +0300

    add makefile and hearbeat and version handlers

--- server/echo.go
+++ server/echo.go
@@ -2,8 +2,9 @@ package server
 
 import (
 	"context"
-	"github.com/labstack/echo/v4/middleware"
 	"net/http"
+
+	"github.com/labstack/echo/v4/middleware"
 
 	"github.com/labstack/echo/v4"
 )
@@ -28,26 +29,27 @@ func New(info VersionInfo, port string) *Server {
 
 func (s Server) initHandlers(e *echo.Echo) {
 	e.GET("/", handler)
+	e.GET("/__heartbeat_", heartbeatHandler)
+	e.GET("/__version__", s.versionHandler)
 
 	e.Any("/*", func(c echo.Context) error {
 		return c.NoContent(http.StatusNotFound)
 	})
 }
-
 func handler(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello World, from echo router\n")
 }
 
 func (s Server) Serve(ctx context.Context) error {
 	e := echo.New()
-	e.HideBanner= true
+	e.HideBanner = true
 	e.Use(middleware.Recover())
 	e.Use(middleware.Recover())
 	s.initHandlers(e)
 
 	go func() {
 		e.Logger.Infof("start server on port: %s", s.port)
-		err := e.Start(":"+s.port)
+		err := e.Start(":" + s.port)
 		if err != nil {
 			e.Logger.Errorf("start server error: %v", err)
 		}
@@ -55,4 +57,19 @@ func (s Server) Serve(ctx context.Context) error {
 
 	<-ctx.Done()
 	return e.Shutdown(ctx)
+}
+
+func heartbeatHandler(c echo.Context) error {
+	return c.NoContent(http.StatusOK)
+}
+
+func (s Server) versionHandler(c echo.Context) error {
+	return c.JSON(
+		http.StatusOK,
+		map[string]string{
+			"version": s.VersionInfo.Version,
+			"commit":  s.VersionInfo.Commit, // можно отобразить git rev-parse HEAD
+			"build":   s.VersionInfo.Build,  // build — заглушка на будущее
+		},
+	)
 }
